d51d8c287ce1b2adcf5108feb9a679a9a94bca6a
intananindika <intananindika@student.telkomuniversity.ac.id>
2021-12-28 11:44:37 +0700

    2

--- src/tubes_rental/Kendaraan.java
+++ src/tubes_rental/Kendaraan.java
@@ -10,5 +10,75 @@ package tubes_rental;
  * @author A S U S
  */
 public class Kendaraan {
+    int idKendaraan;
+    private String noMesin, noRangka, noPol, merk, warna, kondisi;
+    private int harga;
     
+    //Kendaraan (int idKendaraan, String noMesin, String noRangka, String noPol, String merk, String, warna, String kondisi){
+        
+    //}
+    
+    public void setIdKendaraan(int idKendaraan){
+        this.idKendaraan = idKendaraan;
+    }
+
+    public void setNoMesin(String noMesin) {
+        this.noMesin = noMesin;
+    }
+
+    public void setNoRangka(String noRangka) {
+        this.noRangka = noRangka;
+    }
+
+    public void setNoPol(String noPol) {
+        this.noPol = noPol;
+    }
+    
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+    
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public void setKondisi(String kondisi) {
+        this.kondisi = kondisi;
+    }
+    
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getIdKendaraan() {
+        return idKendaraan;
+    }
+
+    public String getNoMesin() {
+        return noMesin;
+    }
+
+    public String getNoRangka() {
+        return noRangka;
+    }
+
+    public String getNoPol() {
+        return noPol;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public String getKondisi() {
+        return kondisi;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
 }
--- src/tubes_rental/Mobil.java
+++ src/tubes_rental/Mobil.java
@@ -10,5 +10,26 @@ package tubes_rental;
  * @author A S U S
  */
 public class Mobil {
+    int jumlahKursi;
+    private String jenisMobil;
     
+    //Mobil (int idKendaraan, String noMesin, String noRangka, String noPol, String merk, String, warna, String kondisi){
+        
+    //}
+
+    public void setJumlahKursi(int jumlahKursi) {
+        this.jumlahKursi = jumlahKursi;
+    }
+
+    public void setJenisMobil(String jenisMobil) {
+        this.jenisMobil = jenisMobil;
+    }
+
+    public int getJumlahKursi() {
+        return jumlahKursi;
+    }
+
+    public String getJenisMobil() {
+        return jenisMobil;
+    }
 }
