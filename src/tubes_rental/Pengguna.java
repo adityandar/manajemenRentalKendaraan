358af0c253a2583736f8627fff2a230c75a15f24
adityandar <adityandar@gmail.com>
2021-12-28 11:39:04 +0700

    Class Pengguna finish

--- src/tubes_rental/Pengguna.java
+++ src/tubes_rental/Pengguna.java
@@ -10,10 +10,51 @@ package tubes_rental;
  * @author asus
  */
 public class Pengguna {
-
+    
     private String nama;
     private String alamat;
     private String username;
     private String password;
     
+    public Pengguna(String nama, String alamat, String username, String password) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.username = username;
+        this.password = password;
+    }
+
+    
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
