82b96df527b3ffd25cfd88ba1d16668f2576a9b6
Aditya <adityandar@gmail.com>
2021-12-28 11:30:13 +0700

    Class pengguna added

---
+++ src/tubes_rental/Pengguna.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes_rental;
+
+/**
+ *
+ * @author asus
+ */
+public class Tubes_Rental {
+
+    private string nama;
+    private string alamat;
+    private string username;
+    private string password;
+    
+}
